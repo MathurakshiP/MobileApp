3e54cf3b00ed4282101ed2b65e2fe865fbcaf4ff
Imara Mohideen <imara.biz02@gmail.com>
2025-01-13 20:32:39 +0530

    firestore added

--- lib/Screens/widget_tree.dart
+++ lib/Screens/widget_tree.dart
@@ -1,9 +1,8 @@
-
 import 'package:flutter/material.dart';
 import 'package:mobile_app/Screens/get_started.dart';
 import 'package:mobile_app/Screens/home_screen.dart';
 import 'package:mobile_app/auth.dart';
-
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class WidgetTree extends StatefulWidget {
   const WidgetTree({super.key});
@@ -13,13 +12,41 @@ class WidgetTree extends StatefulWidget {
 }
 
 class _WidgetTreeState extends State<WidgetTree> {
+  Future<Map<String, dynamic>?> getUserData(String userId) async {
+    try {
+      DocumentSnapshot userDoc = await FirebaseFirestore.instance.collection('users').doc(userId).get();
+      if (userDoc.exists) {
+        return userDoc.data() as Map<String, dynamic>;
+      } else {
+        return null; // No user data found
+      }
+    } catch (e) {
+      debugPrint('Error fetching user data: $e');
+      return null;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return StreamBuilder(
       stream: Auth().authStateChanges,
       builder: (context, snapshot) {
         if (snapshot.hasData) {
-          return const HomeScreen(); // Redirect to home if user is logged in
+          final user = snapshot.data;
+          return FutureBuilder(
+            future: getUserData(user!.uid),
+            builder: (context, AsyncSnapshot<Map<String, dynamic>?> userSnapshot) {
+              if (userSnapshot.connectionState == ConnectionState.waiting) {
+                return const Center(child: CircularProgressIndicator()); // Show a loading spinner while data is fetched
+              } else if (userSnapshot.hasError) {
+                return Center(child: Text('Error: ${userSnapshot.error}')); // Show error message if something goes wrong
+              } else if (userSnapshot.hasData) {
+                return HomeScreen(userData: userSnapshot.data!); // Pass user data to HomeScreen
+              } else {
+                return const GetStartedScreen(); // Show GetStartedScreen if no user data is found
+              }
+            },
+          );
         } else {
           return const GetStartedScreen(); // Redirect to GetStartedScreen if user is not logged in
         }
