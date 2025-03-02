96edd64112fe6c04468224e25793ce7181bf2414
Kathushan12 <kathu12shan@gmail.com>
2025-03-02 17:37:49 +0530

    Email dlted

--- lib/Screens/help_support_screen.dart
+++ lib/Screens/help_support_screen.dart
@@ -16,20 +16,6 @@ class HelpSupportScreen extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              // Contact Support Section
-              const SizedBox(height: 16),
-              const Text(
-                'Contact Support',
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-              ),
-              ListTile(
-                leading: Icon(Icons.email),
-                title: const Text('Email Us'),
-                onTap: () {
-                  // Logic to open email client
-                },
-              ),
-
               // Report a Problem Section
               const SizedBox(height: 16),
               const Text(
